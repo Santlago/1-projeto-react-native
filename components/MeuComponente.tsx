f64f61f7656174da03a3adf9cfca3849458e67c0
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-15 09:45:24 -0300

    mobile project

---
+++ components/MeuComponente.tsx
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from "react-native";
+
+interface Props {
+  text: String;
+}
+
+export function MeuComponente({ text }: Props) {
+  return (
+    <View>
+      <Text style={styles.myStyle}>{text}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  myStyle: {
+    color: "white",
+  },
+});
